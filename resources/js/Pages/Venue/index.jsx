9bcf553f3cb3185f7d2f05ac0494ee24e75c6a46
grifis <fujeba2abm@i.softbank.jp>
2022-08-29 17:29:47 +0900

    チャット欄

--- resources/js/Pages/Venue/index.jsx
+++ resources/js/Pages/Venue/index.jsx
@@ -35,27 +35,27 @@ const Index = (props) => {
                         venueId={venue.id}
                     />
                 ))}
-                <VenueTestIndex
-                    image={Camp}
-                    color="border-yellow-200"
-                    status="東京"
-                    description="新宿御苑でピクニックをしましょう！現在企画中！一緒に盛り上げてくれる仲間を探してます！"
-                    name="若林正恭"
-                />
-                <VenueTestIndex
-                    image={Fes}
-                    color="border-green-200"
-                    status="栃木"
-                    description="中規模な音楽フェスを関東で開きたいと考えています！開催地の提供をしてくださる方を募集しています！"
-                    name="春日俊彰"
-                />
-                <VenueTestIndex
-                    image={Fireworks}
-                    color="border-red-200"
-                    status="静岡"
-                    description="9/15に花火大会を熱海で開催します！25分間の水上花火を打ち上げます！ぜひ見にきてください！"
-                    name="有吉弘行"
-                />
+                {/*<VenueTestIndex*/}
+                {/*    image={Camp}*/}
+                {/*    color="border-yellow-200"*/}
+                {/*    status="東京"*/}
+                {/*    description="新宿御苑でピクニックをしましょう！現在企画中！一緒に盛り上げてくれる仲間を探してます！"*/}
+                {/*    name="若林正恭"*/}
+                {/*/>*/}
+                {/*<VenueTestIndex*/}
+                {/*    image={Fes}*/}
+                {/*    color="border-green-200"*/}
+                {/*    status="栃木"*/}
+                {/*    description="中規模な音楽フェスを関東で開きたいと考えています！開催地の提供をしてくださる方を募集しています！"*/}
+                {/*    name="春日俊彰"*/}
+                {/*/>*/}
+                {/*<VenueTestIndex*/}
+                {/*    image={Fireworks}*/}
+                {/*    color="border-red-200"*/}
+                {/*    status="静岡"*/}
+                {/*    description="9/15に花火大会を熱海で開催します！25分間の水上花火を打ち上げます！ぜひ見にきてください！"*/}
+                {/*    name="有吉弘行"*/}
+                {/*/>*/}
                 {/* イベント一覧ここまで */}
                 {/* article - end */}
             </div>
